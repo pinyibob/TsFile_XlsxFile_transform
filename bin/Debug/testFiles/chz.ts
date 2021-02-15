790730edbcdc074aef3512ea7d02517d3850358e
pinyibob <53402982+pinyibob@users.noreply.github.com>
2021-02-16 03:03:05 +0800

    Add files via upload

---
+++ bin/Debug/testFiles/chz.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>fr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>en</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>chz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>hello world! always the bese wishes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TestClass</name>
+    <message>
+        <source>testString</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>testString!!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>aTestCppFile</name>
+    <message>
+        <source>testText</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ bin/Debug/testFiles/fr.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="40"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="55"/>
+        <source>fr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="60"/>
+        <source>en</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>chz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="36"/>
+        <source>hello world! always the bese wishes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TestClass</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>testString</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="46"/>
+        <source>testString!!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>aTestCppFile</name>
+    <message>
+        <location filename="../atestcppfile.cpp" line="7"/>
+        <source>testText</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
